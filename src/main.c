4ad77b9dbd5b08338b1f31a310e7ad044d69ad4c
Fikri Naufal Hamdi <16521293@mahasiswa.itb.ac.id>
2022-11-02 17:30:49 +0700

    Update main.c (take directory argument functionality in LOAD command)

--- src/main.c
+++ src/main.c
@@ -19,7 +19,7 @@ int main(){
     while (!EndProgram)
     {
         printf("\nENTER COMMAND: ");
-        scanf("%s", command);
+        scanf("%[^\n]s", command);
         if (IsEmpty(GamesList))
         {
             if (compareString(command, "START"))
@@ -29,12 +29,22 @@ int main(){
 
             else if (compareString(command, "LOAD"))
             {
+                // Take directory argument
+                char directory[100];
+                int i = 0;
+                while (command[i+5] != '\0')
+                {
+                    directory[i] = command[i+5];
+                    i++;
+                }
+
                 // LOAD
             } 
 
             else {
                 printf("Belum ada konfigurasi games yang dimuat\n");
                 printf("Muat konfigurasi games terlebih dahulu dengan command LOAD <filename>\n");
+                printf("%s\n",command);
             }
         }
     }
