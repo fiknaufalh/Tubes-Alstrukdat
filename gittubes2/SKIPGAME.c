9152e94bfd6b673f73cb77e0b3e38b5c59610e5a
Ghaylan <102856179+Mikdar1@users.noreply.github.com>
2022-11-04 19:03:51 +0700

    Update SKIPGAME.c

--- gittubes2/SKIPGAME.c
+++ gittubes2/SKIPGAME.c
@@ -4,35 +4,38 @@
 #include <math.h>
 #include <time.h>
 #include "SKIPGAME.h"
-void SKIPGAME(Queue q, Queue q2, int input){    
+void SKIPGAME(Queue q, Queue q2, Queue history, int input){    
     time_t t;
     boolean status = true;
     srand((unsigned) time(&t));
     printf("Berikut adalah daftar Game-mu\n");
-    for(int j = 0;j<length(q);j++){
-        printf("%d. %s\n",(j+1),q.buffer[j]);
+    int j =0;
+    int a = length(q);
+    for(a;a>0;a--){
+        printf("%d. %s\n",(j+1),q.buffer[a-1]);
+        j++;
     }
     if (input+1 <= 0 || input+1>length(q)){
-            printf("salah\n");
+            printf("Tidak ada permainan lagi dalam daftar game-mu atau input < 0.\n");
     }
     else{
         int i =0;
         while(i<length(q2) && status){
             if ((q.buffer[input]==q2.buffer[i])){
                 if (i>1){
-                    printf("Loading %s ...\n", q.buffer[input]);
+                    printf("Loading %s ...\n", q.buffer[length(q) - input-1]);
                     printf("%d\n", rand());
                     status = false;
                     break;
                 }
                 else if (i==1){
-                    printf("Loading %s ...\n", q.buffer[input]);
+                    printf("Loading %s ...\n", q.buffer[length(q) - input-1]);
                     printf("diner");
                     status = false;
                     break;
                 }
                 else if (i==0 ){
-                    printf("Loading %s ...\n", q.buffer[input]);
+                    printf("Loading %s ...\n", q.buffer[length(q) - input-1]);
                     printf("rng");
                     status = false;
                     break;
@@ -41,7 +44,15 @@ void SKIPGAME(Queue q, Queue q2, int input){
             i++;
         }
         if (status){
-            printf("Game %s masih dalam maintenance, belum dapat dimainkan. Silahkan pilih game lain.\n", q.buffer[input]);
+            printf("Game %s masih dalam maintenance, belum dapat dimainkan. Silahkan pilih game lain.\n", q.buffer[ length(q) - input-1]);
         }
+        ElType val;
+        i=0;
+        while (i<input+1){
+            dequeue(&q,&val);
+            enqueue(&history, val);
+            i++;
+        }
+        
     }
 }
