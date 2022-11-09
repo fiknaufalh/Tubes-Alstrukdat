ba3f10f1c142a38ad2830299f5b71de0a907f780
Ghaylan <102856179+Mikdar1@users.noreply.github.com>
2022-11-09 19:15:02 +0700

    Update PLAYGAME.c

--- gittubes2/PLAYGAME.c
+++ gittubes2/PLAYGAME.c
@@ -5,45 +5,50 @@
 #include <time.h>
 #include "PLAYGAME.h"
 
-void PLAYGAME(Queue q, Queue q2, Queue history){
-    time_t t;
-    boolean status = true;
-    srand((unsigned) time(&t));
-    printf("Berikut adalah daftar Game-mu\n");
-    int j =0;
-    int a = length(q);
-    for(a;a>0;a--){
-        printf("%d. %s\n",(j+1),q.buffer[a-1]);
-        j++;
-    }
-    int i =0;
-    while(i<length(q2) && status){
-        if ((TAIL(q)==q2.buffer[i])){
-            if (i>1){
-                printf("Loading %s ...\n", TAIL(q));
-                printf("%d\n", rand());
-                status = false;
-                break;
-            }
-            else if (i==1){
-                printf("Loading %s ...\n", TAIL(q));
-                printf("diner");
-                status = false;
-                break;
-            }
-            else if (i==0 ){
-                printf("Loading %s ...\n", TAIL(q));
-                printf("rng");
-                status = false;
-                break;
-            }
+void PLAYGAME(Queue *q, Queue q2, Queue *history){
+    if(!isEmpty(*q)){
+        time_t t;
+        boolean status = true;
+        srand((unsigned) time(&t));
+        printf("Berikut adalah daftar Game-mu\n");
+        int j =0;
+        int a = length(*q);
+        for(int i=0; i<a; i++){
+            printf("%d. %s\n",(j+1),(*q).buffer[i]);
+            j++;
         }
-        i++;
+        int i =0;
+        while(i<length(q2) && status){
+            if ((HEAD(*q)==q2.buffer[i])){
+                if (i>1){
+                    printf("Loading %s ...\n", HEAD(*q));
+                    printf("%d\n", rand());
+                    status = false;
+                    break;
+                }
+                else if (i==1){
+                    printf("Loading %s ...\n", HEAD(*q));
+                    printf("diner\n"); // MANGGIL DINER
+                    status = false;
+                    break;
+                }
+                else if (i==0 ){
+                    printf("Loading %s ...\n", HEAD(*q));
+                    printf("rng\n"); //MANGGIL RNG
+                    status = false;
+                    break;
+                }
+            }
+            i++;
+        }
+        if (status){
+            printf("Game %s masih dalam maintenance, belum dapat dimainkan.\n", HEAD(*q));
+        }
+        ElType val;
+        dequeue(q,&val);
+        enqueue(history,val);
     }
-    if (status){
-            printf("Game %s masih dalam maintenance, belum dapat dimainkan.\n", TAIL(q));
-    }
-    ElType val;
-    dequeue(&q,&val);
-    enqueue(&history,val);
+    else{
+        printf("Anda belum men-queue game apapun!\n");
+    } 
 }
