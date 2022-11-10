fcbb29b33e642817c5117af998cd5566b100ec7c
Fikri Naufal Hamdi <16521293@mahasiswa.itb.ac.id>
2022-11-10 13:38:45 +0700

    Delete unnecessary files

--- DinerDash/main2.c
+++
@@ -1,6 +0,0 @@
-#include "DinerDash.h"
-
-int main(){
-    DinerDash();
-    return 0;
-}
--- gittubes2/SKIPGAME.h
+++
@@ -1,6 +0,0 @@
-#ifndef SKIPGAME_H
-#define SKIPGAME_H
-
-#include "queue.h"
-void SKIPGAME(Queue *q, Queue q2,Queue *history, int input);
-#endif
