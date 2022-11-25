21f0d9fab487f301239df174355e18c1a3175573
Fikri Naufal Hamdi <16521293@mahasiswa.itb.ac.id>
2022-11-26 01:09:50 +0700

    Update games.h (Hangman, ToH, SoM)

--- src/Games/games.h
+++ src/Games/games.h
@@ -1,7 +1,10 @@
 #ifndef __GAMES_H__
 #define __GAMES_H__
 
-#include "DinerDash/DinerDash.h"
 #include "RNG/RNG.h"
+#include "DinerDash/DinerDash.h"
+#include "Hangman/Hangman.h"
+#include "Tower_of_Hanoi/ToH.h"
+#include "Snake_on_Meteor/SoM.h"
 
 #endif
